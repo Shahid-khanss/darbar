fe1d85fcf3db6966efd078bd6191bb07d901c6d5
shahid <shahidkhan110044@gmail.com>
2023-11-10 16:21:32 +0530

    3

--- src/TableCart.jsx
+++ src/TableCart.jsx
@@ -20,11 +20,11 @@ function handleAddItems(e){
     return ( 
         <div className="table-cart">
             {tCart.map((item,index)=>{
-                return ( <>
+                return ( <div className="item-row">
                     <div className="sno">{index+1}</div>
                     <div className="itemno">{item}</div>
                     <div className="delete">delete</div>
-                    </>             
+                    </div>             
                 )
             })}
         <a onClick={handleAddItems} href="" className="add-items">Add Items</a>
